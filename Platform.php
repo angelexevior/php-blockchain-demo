39406c6ebdd489ce67b8cf36a15cb9f34c36bad2
angelexevior <exevior@localhost>
2020-10-27 14:15:48 +0200

    Core blockchain class with comments

--- Platform.php
+++ Platform.php
@@ -11,27 +11,31 @@ class Platform {
 
     public $db;
     public $config;
-
+    
+    //Our constructor 
+    //Initializes the chain stdclass (object) with the genesis block
     public function __construct() {
         $this->chain = [$this->createGenesisBlock()];
-        $this->difficulty = 2;
-      
-
+        $this->difficulty = 4;
     }
 
-    
+    //Generate genesis block
     private function createGenesisBlock() {
         return new Block(0, strtotime("2017-01-01"), "Genesis Block");
     }
 
+    //Get the last block from the chain
     public function getLastBlock() {
         return $this->chain[count($this->chain) - 1];
     }
     
+    //Get entire chain
     public function getChain(){
         return $this->chain;
     }
 
+    //Push new block
+    //Usage push(new Block(index,timestamp,data));
     public function push($block) {
         
         $block->previousHash = $this->getLastBlock()->hash;
@@ -39,7 +43,11 @@ class Platform {
         array_push($this->chain, $block);
     }
 
+    //Used above by push()
     public function mine($block) {
+        //Sample difficulty algorithm
+        //Keep generating new hash until has begins with 0000 
+        //(How many zeroes depends on difficulty specified in constructor)
         while (substr($block->hash, 0, $this->difficulty) !== str_repeat("0", $this->difficulty)) {
             $block->nonce++;
             $block->hash = $block->calculateHash();
@@ -49,6 +57,7 @@ class Platform {
         echo "Block mined: " . $block->hash . "\n";
     }
 
+    //Validate the chain hasn't been altered or malformed
     public function isValid() {
         for ($i = 1; $i < count($this->chain); $i++) {
             $currentBlock = $this->chain[$i];
